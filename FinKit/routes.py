35ba7c2413bf07e6ac8fb54b8e315ff8814d11a7
Oliverw14 <58699523+Oliverw14@users.noreply.github.com>
2022-04-13 12:01:21 +0100

    added tons

--- FinKit/routes.py
+++ FinKit/routes.py
@@ -4,6 +4,10 @@ from FinKit.models import Accounts, User
 from FinKit.forms import RegisterForm, LoginForm, UserUpdateForm
 from FinKit import db
 from flask_login import login_user, logout_user, login_required
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 
 @app.route('/')
 @app.route('/home')
@@ -48,7 +52,46 @@ def register_page():
 @app.route('/stock')
 @login_required
 def stock_page():
-    return render_template('stockPage.html')
+    df = pd.read_csv('FinKit\data\yield_curve.csv')   
+    df2 = pd.read_csv('FinKit\data\yield_distance.csv')
+    df3 = pd.read_csv('FinKit\data\HSI_sma.csv', usecols=["date", "close_gt_sma50"])
+    sdf3 = df3.sort_values(by='date', ascending=True)
+    df4 = pd.read_csv('FinKit\data\HSI_sma.csv', usecols=["date", "close_gt_sma200"])
+    sdf4 = df4.sort_values(by='date', ascending=True)
+    fig = px.line(df, x='period', y='rate', title='Bond Yield Curve')
+    fig2 = px.line(df2, x='Date', y='difference', title='Bond Yield Distance')
+    fig3 = px.line(sdf3, x='date', y='close_gt_sma50', title='No of stocks > SMA50')
+    fig3.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ]) 
+        )
+    )   
+    fig4 = px.line(sdf4, x='date', y='close_gt_sma200', title='No of stocks > SMA200')
+    fig4.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ]) 
+        )
+    )      
+    graphJSON1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder) 
+    graphJSON3 = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)  
+    graphJSON4 = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)   
+    return render_template('stockPage.html', graphJSON1=graphJSON1, graphJSON2=graphJSON2,graphJSON3=graphJSON3,graphJSON4=graphJSON4)
+
     
 @app.route('/admin')
 def admin_page():
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from FinKit import app
 
 if __name__ == '__main__':
+    #app.run(ssl_context='adhoc')
     app.run(debug=True)
