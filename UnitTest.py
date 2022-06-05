5612ad487deccb699acc5d7cc43187c025d2e7ed
Oliver Wilson <oliver.wilson96@hotmail.com>
2022-06-05 22:14:51 +0100

    selenium tests

--- UnitTest.py
+++ UnitTest.py
@@ -3,12 +3,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
-driver = webdriver.Firefox(firefox_binary=binary, executable_path="C://usr/local/bin/geckodriver")
-driver.get("https://oliverw14.pythonanywhere.com")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+driver = webdriver.Firefox()
+driver.get("https://oliverw14.pythonanywhere.com/register")
+elem = driver.find_element_by_name("username")
+elem.send_keys('Oliverw14')
+elem = driver.find_element_by_name("firstname")
+elem.send_keys('Oliver')
+elem = driver.find_element_by_name("lastname")
+elem.send_keys('Wilson')
+elem = driver.find_element_by_name("email_address")
+elem.send_keys('UnitTest@test.com')
+elem = driver.find_element_by_name("password_1")
+elem.send_keys('password123')
+elem = driver.find_element_by_name("password_2")
+elem.send_keys('password123')
+elem.send_keys(Keys.ENTER)
 assert "No results found." not in driver.page_source
 driver.close()
